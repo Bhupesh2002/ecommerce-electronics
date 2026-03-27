78324ad1c98fe614c02de508e7135535df8bfbd4
Bhupesh Dhesingurajan <rajanbhupesh@gmail.com>
2026-03-27 18:49:48 +0530

    port number added

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.use("/api/orders", orderRoutes);
 
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
