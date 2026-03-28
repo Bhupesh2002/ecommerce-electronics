ecf287337ad3a0e96a995eb2c1c648f3dd4c2f7c
Bhupesh Dhesingurajan <rajanbhupesh@gmail.com>
2026-03-28 11:43:24 +0530

    Authcontext modified

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,8 +3,7 @@ import { FaShoppingCart, FaUser, FaHome } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import { useState,useEffect } from "react";
 import { useContext } from "react";
-import AuthProvider from "../context/authContext";
-
+import AuthProvider from "../context/AuthContext";
 
 const Navbar = () => {
 
